7dc4c46f6b7dfaea979efa8b8b9be2ef8a28a109
anamikajangid <117848984+anamikajangid@users.noreply.github.com>
2023-05-26 22:49:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function volume_sphere() {
     var volume;
-	var radius= document.getElementById("radius").value;
-	radius=Math.abs(radius);
-	volume =(4/3) * Math.PI*Math.Pow(radius, 3);
-	volume = volume.toFixed(4);
-	document.getElementByid("volume").value = volume;	
-	return false;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+  return false;
   
 } 
 
